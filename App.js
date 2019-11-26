bf1c6511c8bb5f6ec790454e957581e6cafc16fd
Kevin Mendez <kevinrmendez@gmail.com>
2019-11-26 23:38:21 +0200

    initial commit

---
+++ App.js
@@ -0,0 +1,170 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+// import React from 'react';
+import React, {useState} from 'react';
+
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  Button,
+  Image,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import YouTube from 'react-native-youtube';
+// import {YouTubeStandaloneAndroid} from 'react-native-youtube';
+
+import youtubeApikey from './apikey.js';
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    // Don't call this.setState() here!
+    this.state = {
+      name: 'Channel Name 1',
+      playListId: ['hY7m5jjJ9mM', 'KVZ-P-ZI6W4', 'Tl0DMTlwLw4'],
+      icon:
+        'https://cdn0.iconfinder.com/data/icons/emoticons-round-smileys/137/Emoticons-01-512.png',
+    };
+    this.playnNextVideo = this.playnNextVideo.bind(this);
+    this.child = React.createRef();
+  }
+  componentDidMount() {
+    // YouTubeStandaloneAndroid.playVideos({
+    //   apiKey: 'AIzaSyBGzR9hDllXVRtDnBVpEmhQsPTw1jc7WbY', // Your YouTube Developer API Key
+    //   videoIds: ['hY7m5jjJ9mM', 'KVZ-P-ZI6W4', 'Tl0DMTlwLw4'], // YouTube video ID
+    //   autoplay: false, // Autoplay the video
+    //   startTime: 120, // Starting point of video (in seconds)
+    // })
+    //   .then(() => console.log('Standalone Player Exited'))
+    //   .catch(errorMessage => console.error(errorMessage));
+    // this.setState({myState: 'Florida'});
+  }
+  playnNextVideo() {
+    // this.setState = {videoId: 'Tl0DMTlwLw4'};
+    this.child.current.nextVideo();
+    // this.child.current.seekTo(20);
+  }
+
+  render() {
+    return (
+      <>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView>
+          <ScrollView
+            contentInsetAdjustmentBehavior="automatic"
+            style={styles.scrollView}>
+            {/* <Header /> */}
+            {global.HermesInternal == null ? null : (
+              <View style={styles.engine}>
+                <Text style={styles.footer}>Engine: Hermes</Text>
+              </View>
+            )}
+            <View style={styles.body}>
+              <View style={styles.header}>
+                <Text style={styles.headerTitle}>{this.state.name}</Text>
+                <Image
+                  style={{width: 80, height: 80}}
+                  source={{
+                    uri: this.state.icon,
+                  }}
+                />
+              </View>
+              <YouTube
+                ref={this.child}
+                videoIds={this.state.playListId} // The YouTube video ID
+                play={true} // control playback of video with true/false
+                fullscreen={false} // control whether the video should play in fullscreen or inline
+                loop={true} // control whether the video should loop when ended
+                apiKey={youtubeApikey}
+                onReady={e => this.setState({isReady: true})}
+                onChangeState={e => {
+                  console.log(e.state);
+
+                  this.setState({status: e.state});
+                }}
+                // onChangeQuality={e => this.setState({quality: e.quality})}
+                // onError={e => this.setState({error: e.error})}
+                style={{alignSelf: 'stretch', height: 500}}
+              />
+              <Button title="next" onPress={() => this.playnNextVideo()} />
+
+              <View style={styles.sectionContainer}>
+                <Text style={styles.sectionTitle}>Channels</Text>
+              </View>
+              <View style={styles.sectionContainer}>
+                <Text style={styles.sectionTitle}>Learn Mor dsfe</Text>
+                <Text style={styles.sectionDescription}>
+                  Read the docs to discover what to sdf do next: sdf
+                </Text>
+              </View>
+              <LearnMoreLinks />
+            </View>
+          </ScrollView>
+        </SafeAreaView>
+      </>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  header: {justifyContent: 'center', alignItems: 'center'},
+  headerTitle: {
+    fontSize: 30,
+    textAlign: 'center',
+    color: Colors.white,
+  },
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.black,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.white,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
+
+export default App;
