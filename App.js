ae639cda352f6fb432756ec8dda953ddbcd87276
Kevin Mendez <kevinrmendez@gmail.com>
2019-11-28 16:50:34 +0200

    remove style

--- App.js
+++ App.js
@@ -257,12 +257,7 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     padding: 10,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
+
   highlight: {
     fontWeight: '700',
   },
